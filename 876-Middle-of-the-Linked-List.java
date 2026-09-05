0f8bf5064a52e7bd6f836083f346329d021cf664
Soumojit-Dev <soumojitbanerjee08@gmail.com>
2026-09-05 20:16:53 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 71.77%] - LeetPush

---
+++ 876-Middle-of-the-Linked-List.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int n = 0;
+        while(temp != null){
+            temp = temp.next;
+            n++;
+        }
+
+        int mid = Integer.MIN_VALUE;
+        if (n % 2 == 0) {
+            mid = (n+1) / 2;
+        } else {
+            mid = n/2;
+        }
+
+        temp = head;
+        for(int i=1; i<=mid; i++){
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+}
