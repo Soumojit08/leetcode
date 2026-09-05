f6776b38ff70a823818734aaee0453d8b427591e
Soumojit-Dev <soumojitbanerjee08@gmail.com>
2026-09-05 20:37:06 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 16.02%] - LeetPush

--- 876-Middle-of-the-Linked-List.java
+++ 876-Middle-of-the-Linked-List.java
@@ -10,25 +10,37 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp = head;
-        int n = 0;
-        while(temp != null){
-            temp = temp.next;
-            n++;
-        }
+        // ListNode temp = head;
+        // int n = 0;
+        // while(temp != null){
+        //     temp = temp.next;
+        //     n++;
+        // }
 
-        int mid = Integer.MIN_VALUE;
-        if (n % 2 == 0) {
-            mid = (n+1) / 2;
-        } else {
-            mid = n/2;
-        }
+        // int mid = Integer.MIN_VALUE;
+        // if (n % 2 == 0) {
+        //     mid = (n+1) / 2;
+        // } else {
+        //     mid = n/2;
+        // }
 
-        temp = head;
-        for(int i=1; i<=mid; i++){
-            temp = temp.next;
-        }
+        // temp = head;
+        // for(int i=1; i<=mid; i++){
+        //     temp = temp.next;
+        // }
 
-        return temp;
+        // return temp;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null) {
+            fast = fast.next; // first 1 step 
+            if (fast != null) { // if fast not null in next step then go another step
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return slow;
     }
 }
